fdb57131f096563a2d3162781b3a49b859435f36
Serjkea <ser.kost@mail.ru>
2020-06-28 22:30:09 +0300

    added files

--- springBootDemo/src/main/java/ru/practics/springbootdemo/MyProperties.java
+++
@@ -1,8 +0,0 @@
-package ru.practics.springbootdemo;
-
-import org.springframework.boot.context.properties.ConfigurationProperties;
-
-@ConfigurationProperties("listener")
-public class MyProperties {
-    boolean flag;
-}
